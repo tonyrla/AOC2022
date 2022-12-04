69f082c72aa7ac8acc3379a9d5386d427789a814
Tony <65010972+tonyrla@users.noreply.github.com>
2022-12-04 08:18:03 +0200

    day04: slightly optimized, slightly more(or less?) readable

--- day04.py
+++ day04.py
@@ -5,42 +5,37 @@ from AOCRla.aoc import AOC
 class puzzle(AOC):
     def __init__(self, open_browser: bool = False):
         super().__init__(open_browser=open_browser)
-        self.input = self.get_input_data()
+        self.input: str = self.get_input_data()
+
+    def _get_ranges(self, line: str) -> tuple[range, range]:
+        start_a, end_a, start_b, end_b = tuple(map(int,line.replace(',', '-').split('-')))
+        set_a = range(start_a, end_a+1)
+        set_b = range(start_b, end_b+1)
+        return set_a, set_b
 
     def part1(self) -> int|None:
-        input = self.get_input_data().splitlines()
+        input = self.input.splitlines()
         count = 0
         for line in input:
-            a,b = line.split(',')
-            start_a, end_a = a.split('-')
-            start_b, end_b = b.split('-')
-            set_a = range(int(start_a), int(end_a)+1)
-            set_b = range(int(start_b), int(end_b)+1)
+            set_a, set_b = self._get_ranges(line)
             intersections = len(set(set_a).intersection(set_b))
-            if intersections == len(set_a) or intersections == len(set_b):
-                count += 1
+            count += (intersections == len(set_a) or intersections == len(set_b))
 
         return count
 
     def part2(self) -> int|None:
-        input = self.get_input_data().splitlines()
+        input = self.input.splitlines()
         count = 0
         for line in input:
-            a,b = line.split(',')
-            start_a, end_a = a.split('-')
-            start_b, end_b = b.split('-')
-            set_a = range(int(start_a), int(end_a)+1)
-            set_b = range(int(start_b), int(end_b)+1)
-            if len(set(set_a).intersection(set_b)) > 0:
-                count += 1
+            set_a, set_b = self._get_ranges(line)
+            count += len(set(set_a).intersection(set_b)) > 0
         return count
 
     def part1_oneliner(self) -> int|None:
-        return sum([1 if len(pair[0]) == len(set(pair[0]).intersection(pair[1])) or len(pair[1]) == len(set(pair[0]).intersection(pair[1])) else 0 for pair in [(range(pair[0],pair[1]+1), range(pair[2],pair[3]+1)) for pair in [tuple(map(int, pair.replace(',', '-').split('-'))) for pair in [line for line in open(f'./inputs/2022_04.txt').read().strip().splitlines()]]]])
-
+        return sum([len(pair[0]) == len(set(pair[0]).intersection(pair[1])) or len(pair[1]) == len(set(pair[0]).intersection(pair[1])) for pair in [(range(pair[0],pair[1]+1), range(pair[2],pair[3]+1)) for pair in [tuple(map(int, pair.replace(',', '-').split('-'))) for pair in [line for line in open(f'./inputs/2022_04.txt').read().strip().splitlines()]]]])
 
     def part2_oneliner(self) -> int|None:
-        return sum([1 if len(set(pair[0]).intersection(pair[1])) > 0 else 0 for pair in [(range(pair[0],pair[1]+1), range(pair[2],pair[3]+1)) for pair in [tuple(map(int, pair.replace(',', '-').split('-'))) for pair in [line for line in open(f'./inputs/2022_04.txt').read().strip().splitlines()]]]])
+        return sum([len(set(pair[0]).intersection(pair[1])) > 0 for pair in [(range(pair[0],pair[1]+1), range(pair[2],pair[3]+1)) for pair in [tuple(map(int, pair.replace(',', '-').split('-'))) for pair in [line for line in open(f'./inputs/2022_04.txt').read().strip().splitlines()]]]])
 
 if __name__ == '__main__':
     use_browser = False
